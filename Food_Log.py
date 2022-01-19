be6c60984444858788d95414f4831fbff2dc7ad1
Cole Matsueda <cmatsueda@chapman.edu>
2022-01-18 17:56:34 -0800

    Addded comments

---
+++ Food_Log.py
@@ -0,0 +1,7 @@
+#User can input food and calories
+#use can View calories for the day and week
+#app can calculate how much calories the user should be intaking in order to be in a deficit
+#Food is saved into database with the cals
+#can filter by date
+
+
